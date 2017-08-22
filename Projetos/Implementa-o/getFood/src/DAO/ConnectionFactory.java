f8f5d02f6b28df7f1c8565da8a58c29577d11112
ViniPessoa8 <vini.pessoa7@gmail.com>
2017-08-21 20:37:05 -0400

    Criei o pacote DAO e a Classe ConnectionFactory

---
+++ Projetos/Implementa-o/getFood/src/DAO/ConnectionFactory.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package DAO;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+/**
+ *
+ * @author Vinicius
+ */
+public class ConnectionFactory {
+
+    public Connection getConnection() {
+        try {
+            return DriverManager.getConnection(
+                    "jdbc:mysql://localhost/fj21", "root", "root");
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
