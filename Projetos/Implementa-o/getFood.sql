2756520d2da4bdb4783c8996d416c383a5a0cf68
Vinícius Pessoa <vini.pessoa7@gmail.com>
2017-08-22 12:11:07 -0400

    Script para criar o banco de dados

---
+++ Projetos/Implementa-o/getFood.sql
@@ -0,0 +1,61 @@
+create database getFood;
+use getFood;
+
+drop database getFood;
+
+create table aluno(
+matricula float(12) not null,
+primary key(matricula),
+foto longblob,
+saldo float,
+nome varchar(50) not null,
+curso varchar(25) not null,
+foreign key(curso) references curso(codigo),
+turma varchar(25) not null,
+foreign key (turma) references turma(codigo)
+);
+create table funcionario(
+matricula float(12) not null,
+primary key(matricula),
+cargo varchar(25) not null,
+nome varchar(50) not null
+);
+create table ficha(
+valor float not null,
+primary key(valor)
+);
+create table venda(
+codigo int not null,
+primary key(codigo),
+matrAl float(12) not null,
+foreign key(matrAl) references aluno(matricula),
+matrFun float(12) not null,
+foreign key(matrFun) references funcionario(matricula),
+valor float not null,
+dt date not null
+);
+create table relatorio
+(
+dt date not null,
+codigo int not null,
+primary key(codigo),
+totalSaldo float not null,
+totalAlunos int not null,
+codVenda int not null,
+foreign key(codVenda) references venda(codigo)
+);
+
+create table curso
+(
+codigo varchar(25) not null,
+primary key(codigo),
+nome varchar(50) not null
+);
+create table turma
+(
+codigo varchar(25) not null,
+primary key(codigo),
+curso varchar(25) not null,
+foreign key (curso) references curso(codigo)
+);
+select * from funcionario;
