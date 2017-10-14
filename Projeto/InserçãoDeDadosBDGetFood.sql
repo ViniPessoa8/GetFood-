9fd4f74c6f6deaeeb5b49ea5da2b692ff48c859d
Vinícius Pessoa <vini.pessoa7@gmail.com>
2017-10-14 07:58:04 -0400

    alterações na inserção de dados no bd

--- Projeto/InserçãoDeDadosBDGetFood.sql
+++ Projeto/InserçãoDeDadosBDGetFood.sql
@@ -8,11 +8,14 @@ INSERT INTO turma(codigo,curso) VALUES ('IINF31B', '1');
 INSERT INTO aluno(matricula,nome,saldo,turma,curso,foto,beneficiario) VALUES ('201511440260', 'Vinícius Cavalcante de Brito Pessoa',0,'IINF31B','1',null,0);
 INSERT INTO funcionario(matricula,cargo,nome) VALUES ('123456789','Coordenador','Jucibs');
 INSERT INTO login(login,senha,matrFun) VALUES ('admin',MD5('admin'),'123456789');
-
+INSERT INTO ficha(valor) VALUES (5.00);
 INSERT INTO venda(matrAl, matrFun, valor, tipo, dt) VALUES ('201511440260', '123456789', 20.0, 0, '2017-10-02');
 SELECT * FROM aluno WHERE nome LIKE '%a%';
 SELECT * FROM funcionario;
 
 select * from aluno;
 select * from login;
+select * from funcionario;
+select * from ficha;
+select * from venda;
 update aluno set saldo=15 where matricula = 201511440260;
